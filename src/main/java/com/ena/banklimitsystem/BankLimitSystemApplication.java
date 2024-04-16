95f64668a35571556b2bc974cd8b58ab95c91bac
ena02 <erkembekov.nurzhan@mail.ru>
2024-04-16 23:46:46 +0500

    Initial commit

---
+++ src/main/java/com/ena/banklimitsystem/BankLimitSystemApplication.java
@@ -0,0 +1,13 @@
+package com.ena.banklimitsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankLimitSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankLimitSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ena/banklimitsystem/BankLimitSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ena.banklimitsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankLimitSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
