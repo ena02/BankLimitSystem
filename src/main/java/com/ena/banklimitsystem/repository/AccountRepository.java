c1a9f58d8a8ea8b89c1e9d1fe796e3e49d2deea5
ena02 <erkembekov.nurzhan@mail.ru>
2024-04-17 21:02:28 +0500

    remove unnecessary code and object

--- src/main/java/com/ena/banklimitsystem/repository/AccountRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ena.banklimitsystem.repository;
-
-import com.ena.banklimitsystem.model.AccountEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AccountRepository extends JpaRepository<AccountEntity, Integer> {
-}
