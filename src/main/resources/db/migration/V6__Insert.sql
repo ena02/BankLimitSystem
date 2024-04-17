e5fbff4bf33eef390eb99b07b01fefb126e479d9
ena02 <erkembekov.nurzhan@mail.ru>
2024-04-17 21:01:18 +0500

    add insert migration

---
+++ src/main/resources/db/migration/V6__Insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO account (currency_shortname, balance)
+VALUES
+    ('USD', 1000.00),
+    ('KZT', 50000.00),
+    ('RUB', 75000.00);
+
+INSERT INTO expense_category (category_name)
+VALUES
+    ('Product'),
+    ('Service');
